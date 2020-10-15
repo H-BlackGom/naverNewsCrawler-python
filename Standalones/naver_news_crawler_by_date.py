9244b963dec0414c0d57aa47aac161337d1c5536
이형로 <hblackgom@xinapse.ai>
2020-10-15 10:19:05 +0900

    local test

--- Standalones/naver_news_crawler_by_date.py
+++ Standalones/naver_news_crawler_by_date.py
@@ -12,11 +12,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 
 # chrome driver setting.
-# driver_path = '../Driver/chromedriver'
-driver_path = '/usr/local/bin/chromedriver'
+driver_path = '../Driver/chromedriver'
+# driver_path = '/usr/local/bin/chromedriver'
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36")
@@ -26,7 +28,8 @@ driver.implicitly_wait(3)
 
 pattern_publisher = r"\s?[가-힣\s]{3,}기자"
 pattern_email = r"([\w-]+)@([\w\.-]+)(\.[\w\.]+)"
-url = "https://search.naver.com/search.naver?where=news&query={0}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=2&ds=&de=&docid=&nso=so%3Ar%2Cp%3A1m%2Ca%3Aall&mynews=0&refresh_start=0&related=0"
+# url = "https://search.naver.com/search.naver?where=news&query={0}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=2&ds=&de=&docid=&nso=so%3Ar%2Cp%3A1m%2Ca%3Aall&mynews=0&refresh_start=0&related=0"
+url = "https://search.naver.com/search.naver?where=news&query={0}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=3&ds=2020.09.28&de=2020.10.12&docid=&nso=so%3Ar%2Cp%3Afrom20200928to20201012%2Ca%3Aall&mynews=0&refresh_start=0&related=0"
 
 conn = MongoClient('192.168.0.12', 27017)
 category_collection = conn['Changsung']['stock_category']
@@ -35,6 +38,9 @@ save_collection = conn['Changsung']['news']
 comapny_infos = list(category_collection.find({}))
 df = pd.DataFrame(comapny_infos)
 
+except_company = ['포스코케미칼', '더블유게임즈', '우리금융지주', 'HDC현대산업개발', '애경산업', '셀트리온',
+                  '쿠쿠홈시스', 'SK케미칼', 'BGF리테일', '카카오', '오리온', '넷마블', '현대중공업지주', '두산밥캣',
+                  '삼성바이오로직스', '동서']
 company_info = list(zip(*map(df.get, df[['code', 'company', 'business_code', 'business']])))
 print(comapny_infos)
 
@@ -47,6 +53,9 @@ for idx, (code, company, business_code, business) in tqdm(enumerate(company_info
     page_num = 1
     search_url = url.format(company)
     driver.get(search_url)
+
+    if company in except_company:
+        continue
 
     while page_num <= 50:
         html = driver.page_source
@@ -106,6 +115,7 @@ for idx, (code, company, business_code, business) in tqdm(enumerate(company_info
                 }, ignore_index=True)
                 print("title-{0}, date-{1}, press-{2}, company-{3}".format(title, p_date, press, company))
                 time.sleep(random.randrange(3, 10))
+                driver.back()
 
         page_num += 1
         if len(driver.find_elements_by_class_name('next')) > 0:
@@ -116,4 +126,4 @@ for idx, (code, company, business_code, business) in tqdm(enumerate(company_info
 
     if len(tmp_df) > 0:
         save_collection.insert_many(tmp_df.to_dict('records'))
-    driver.quit()
+driver.quit()
